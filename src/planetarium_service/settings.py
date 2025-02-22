d9aaee9ae96969203eeb6e2a439e81def6181bbf
10kkyvl <ztkkre@gmail.com>
2025-02-22 17:49:29 +0200

    feat: added admin and changed a bit urls

--- src/planetarium_service/settings.py
+++ src/planetarium_service/settings.py
@@ -86,6 +86,10 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ),
+    "DEFAULT_RENDERER_CLASSES": (
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.AdminRenderer",
+    )
 }
 
 SIMPLE_JWT = {
--- src/planetarium_service/urls.py
+++ src/planetarium_service/urls.py
@@ -23,8 +23,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from planetarium_api.views import RegisterViewSet
 
+
 router = DefaultRouter()
-router.register(r"register", RegisterViewSet)
+router.register(r"user/register", RegisterViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -39,8 +40,8 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc"
     ),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/user/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/user/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include(router.urls)),
 ]
 
