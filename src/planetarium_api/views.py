86799a73cb8f1f032927210c25d11132bd528457
10kkyvl <ztkkre@gmail.com>
2025-02-22 17:20:40 +0200

    feat: added jwt with docs

--- src/planetarium_api/views.py
+++ src/planetarium_api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from planetarium_api.serializers import RegisterSerializer
+
+
+class RegisterViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = (AllowAny,)
+    http_method_names = ["post"]
--- src/planetarium_service/urls.py
+++ src/planetarium_service/urls.py
@@ -16,8 +16,32 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularRedocView, SpectacularAPIView
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from planetarium_api.views import RegisterViewSet
+
+router = DefaultRouter()
+router.register(r"register", RegisterViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui"
+    ),
+    path(
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc"
+    ),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/", include(router.urls)),
 ]
+
+app_name = "planetarium_api"
